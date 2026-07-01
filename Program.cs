1c602b8e3c66499af0bd823a3ccc91c968d26d0a
if-space <dima.shoto22@gmail.com>
2026-07-01 16:47:00 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,15 +6,15 @@ namespace Lessons
     {
         static void Main(string[] args)
         {
-            string[] names = new string[0];
-            string[] positions = new string[0];
-            bool isOpen = true;
-
             const string AddDossierCommand = "1";
             const string OutputAllDossiersCommand = "2";
             const string DeleteDossiersCommand = "3";
             const string FindDossiersCommand = "4";
             const string ExitCommand = "5";
+
+            string[] names = new string[0];
+            string[] positions = new string[0];
+            bool isOpen = true;
 
             Console.WriteLine("Меню кадрового учета");
 
@@ -60,105 +60,110 @@ namespace Lessons
                 Console.ReadLine();
                 Console.Clear();
             }
+        }
 
-            static void AddDossier(ref string[] names, ref string[] positions)
+        static void AddDossier(ref string[] names, ref string[] positions)
+        {
+            Console.Write("\nВведите ФИО: ");
+            string userName = Console.ReadLine();
+
+            Console.Write("Введите должность: ");
+            string userPosition = Console.ReadLine();
+
+            names = IncreaseDossier(names, userName);
+            positions = IncreaseDossier(positions, userPosition);
+        }
+
+        static string[] IncreaseDossier(string[] dossier, string newValue)
+        {
+            string[] tempDossier = new string[dossier.Length + 1];
+
+            for (int i = 0; i < dossier.Length; i++)
+                tempDossier[i] = dossier[i];
+
+            tempDossier[tempDossier.Length - 1] = newValue;
+
+            dossier = tempDossier;
+
+            return dossier;
+        }
+
+        static void OutputAllDossiers(string[] names, string[] positions)
+        {
+            for (int i = 0; i < names.Length; i++)
             {
-                Console.Write("\nВведите ФИО: ");
-                string userName = Console.ReadLine();
+                int index = i + 1;
 
-                Console.Write("Введите должность: ");
-                string userPosition = Console.ReadLine();
-
-                names = IncreaseDossier(names);
-                names[names.Length - 1] = userName;
-
-                positions = IncreaseDossier(positions);
-                positions[positions.Length - 1] = userPosition;
+                Console.WriteLine($"{index}. {names[i]} - {positions[i]}.");
             }
+        }
 
-            static string[] IncreaseDossier(string[] dossier)
+        static void DeleteDossiers(ref string[] names, ref string[] positions)
+        {
+            OutputAllDossiers(names, positions);
+
+            Console.WriteLine("Введите номер досье, которое вы хотите удалить");
+
+            if (int.TryParse(Console.ReadLine(), out int dossierNumber))
             {
-                string[] tempDossier = new string[dossier.Length + 1];
+                dossierNumber--;
 
-                for (int i = 0; i < dossier.Length; i++)
-                    tempDossier[i] = dossier[i];
-
-                dossier = tempDossier;
-
-                return dossier;
-            }
-
-            static void OutputAllDossiers(string[] names, string[] positions)
-            {
-                for (int i = 0; i < names.Length; i++)
+                if (dossierNumber < names.Length && dossierNumber >= 0)
                 {
-                    int index = i + 1;
+                    names = DecreaseDossier(names, dossierNumber);
+                    positions = DecreaseDossier(positions, dossierNumber);
 
-                    Console.WriteLine($"{index}. {names[i]} - {positions[i]}.");
-                }
-            }
-
-            static void DeleteDossiers(ref string[] names, ref string[] positions)
-            {
-                OutputAllDossiers(names, positions);
-
-                Console.WriteLine("Введите номер досье, которое вы хотите удалить");
-
-                if (int.TryParse(Console.ReadLine(), out int dossierNumber))
-                {
-                    dossierNumber--;
-
-                    if (dossierNumber < names.Length && dossierNumber >= 0)
-                    {
-                        names = DecreaseDossier(names, dossierNumber);
-                        positions = DecreaseDossier(positions, dossierNumber);
-
-                        Console.Write("Досье успешно удалено");
-                    }
-                    else
-                        Console.Write("Некорректный номер досье");
+                    Console.WriteLine("Досье успешно удалено");
                 }
                 else
-                    Console.Write("Ошибка, введено неверное число");
-            }
-
-            static string[] DecreaseDossier(string[] dossier, int userNumbers)
-            {
-                string[] tempDossiers = new string[dossier.Length - 1];
-
-                for (int i = 0; i < userNumbers; i++)
-                    tempDossiers[i] = dossier[i];
-
-                for (int i = userNumbers + 1; i < dossier.Length; i++)
-                    tempDossiers[i - 1] = dossier[i];
-
-                dossier = tempDossiers;
-
-                return dossier;
-            }
-
-            static void FindDossier(string[] names, string[] positions)
-            {
-                Console.Write("Введите фамилию: ");
-                string userInput = Console.ReadLine();
-
-                bool isPersonFind = false;
-
-                for (int i = 0; i < names.Length; i++)
                 {
-                    char space = ' ';
-                    string[] surnames = names[i].Split(space);
-
-                    if (userInput.ToLower() == surnames[0].ToLower())
-                    {
-                        Console.WriteLine($"Досье найдено {names[i]} - {positions[i]}");
-
-                        isPersonFind = true;
-                    }
+                    Console.Write("Некорректный номер досье");
                 }
+            }
+            else
+            {
+                Console.Write("Ошибка, введено неверное число");
+            }
+        }
 
-                if (isPersonFind == false)
-                    Console.WriteLine($"Досье с фамилией {userInput} не найдено");
+        static string[] DecreaseDossier(string[] dossier, int userNumbers)
+        {
+            string[] tempDossiers = new string[dossier.Length - 1];
+
+            for (int i = 0; i < userNumbers; i++)
+                tempDossiers[i] = dossier[i];
+
+            for (int i = userNumbers + 1; i < dossier.Length; i++)
+                tempDossiers[i - 1] = dossier[i];
+
+            dossier = tempDossiers;
+
+            return dossier;
+        }
+
+        static void FindDossier(string[] names, string[] positions)
+        {
+            Console.Write("Введите фамилию: ");
+            string userInput = Console.ReadLine();
+
+            bool isPersonFind = false;
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                char space = ' ';
+                string[] surnames = names[i].Split(space);
+
+                if (userInput.ToLower() == surnames[0].ToLower())
+                {
+                    Console.WriteLine($"Досье найдено {names[i]} - {positions[i]}");
+
+                    isPersonFind = true;
+                }
+            }
+
+            if (isPersonFind == false)
+            {
+                Console.WriteLine($"Досье с фамилией {userInput} не найдено");
             }
         }
     }
